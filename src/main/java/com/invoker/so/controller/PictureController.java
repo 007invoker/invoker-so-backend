18eee8a340750cf18a6c7ed40a6a9c004a5e0522
invoker <1390499626@qq.com>
2023-07-21 20:16:15 +0800

    invoker search backend

---
+++ src/main/java/com/invoker/so/controller/PictureController.java
@@ -0,0 +1,51 @@
+package com.invoker.so.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.invoker.so.common.BaseResponse;
+import com.invoker.so.common.ErrorCode;
+import com.invoker.so.common.ResultUtils;
+import com.invoker.so.exception.ThrowUtils;
+import com.invoker.so.model.dto.picture.PictureQueryRequest;
+import com.invoker.so.model.entity.Picture;
+import com.invoker.so.service.PictureService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 图片接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/picture")
+@Slf4j
+public class PictureController {
+
+    @Resource
+    private PictureService pictureService;
+
+    /**
+     * 分页获取列表（封装类）
+     *
+     * @param pictureQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page/vo")
+    public BaseResponse<Page<Picture>> listPictureByPage(@RequestBody PictureQueryRequest pictureQueryRequest,
+                                                         HttpServletRequest request) {
+        long current = pictureQueryRequest.getCurrent();
+        long size = pictureQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        String searchText = pictureQueryRequest.getSearchText();
+        Page<Picture> picturePage = pictureService.searchPicture(searchText, current, size);
+        return ResultUtils.success(picturePage);
+    }
+
+
+}
